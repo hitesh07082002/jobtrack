414ea94e1896f63f881c21b12674d3bbb4f128f6
hitesh07082002 <hiteshsadhwani777@gmail.com>
2024-03-14 15:55:34 +0530

    recommit

---
+++ app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddJobPage() {
+  return 
+    <h1 className='text-4xl'>AddJobPage</h1>;
+ 
+}
+
+export default AddJobPage
---
+++ app/(dashboard)/stats/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StatsPage() {
+    return 
+      <h1 className='text-4xl'>StatsPage</h1>;
+   
+  }
+  
+  export default StatsPage
